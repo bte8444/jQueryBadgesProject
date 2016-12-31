067c712e86ae7f3aa46a00fe7664754ec7a23d88
brett <brettandjj2@gmail.com>
2016-12-31 12:43:40 -0500

    Completing course

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,27 @@
 $(function() {
 
   // your code will go here
+  var reportCardUrl = "https://www.codeschool.com/users/2221429.json";
 
+  $.ajax(
+    {
+      url: reportCardUrl,
+      dataType : 'jsonp',
+      success : function(response) {
+          // handle response
+          var $badges = $("#badges");
+          $.each(response.courses.completed, function(index, course) {
+            $badges.append('<div class="course">' +
+                              '<h3>' +
+                                course.title + 
+                              '</h3>' +
+                              '<img src="' + course.badge + '"/>' +
+                              '<a href="' + course.url + '" ' +
+                                  ' class="btn btn-primary" target="_blank" >' +
+                               'See Course</a>' +
+                           '</div>');
+          });
+      }
+    }
+  )
 });
